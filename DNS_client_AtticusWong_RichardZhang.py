5340de9cf0c2060e9a646f6c7e3c0370bc7ef5ab
Richard Zhang <rimzhang@ucdavis.edu>
2026-03-03 23:54:09 -0800

    DNS resolver full implementation and output

--- DNS_client_AtticusWong_RichardZhang.py
+++ DNS_client_AtticusWong_RichardZhang.py
@@ -1,20 +1,3 @@
-"""
-a.root-servers.net	198.41.0.4, 2001:503:ba3e::2:30	Verisign, Inc.
-b.root-servers.net	170.247.170.2, 2801:1b8:10::b	University of Southern California,
-Information Sciences Institute
-c.root-servers.net	192.33.4.12, 2001:500:2::c	Cogent Communications
-d.root-servers.net	199.7.91.13, 2001:500:2d::d	University of Maryland
-e.root-servers.net	192.203.230.10, 2001:500:a8::e	NASA (Ames Research Center)
-f.root-servers.net	192.5.5.241, 2001:500:2f::f	Internet Systems Consortium, Inc.
-g.root-servers.net	192.112.36.4, 2001:500:12::d0d	US Department of Defense (NIC)
-h.root-servers.net	198.97.190.53, 2001:500:1::53	US Army (Research Lab)
-i.root-servers.net	192.36.148.17, 2001:7fe::53	Netnod
-j.root-servers.net	192.58.128.30, 2001:503:c27::2:30	Verisign, Inc.
-k.root-servers.net	193.0.14.129, 2001:7fd::1	RIPE NCC
-l.root-servers.net	199.7.83.42, 2001:500:9f::42	ICANN
-m.root-servers.net	202.12.27.33, 2001:dc3::35	WIDE Project
-"""
-
 import socket
 import sys
 import struct
@@ -26,9 +9,16 @@ ROOT_SERVERS = [
     "170.247.170.2",
     "192.33.4.12",
     "199.7.91.13",
-    "192.203.230.10"
+    "192.203.230.10",
+    "192.5.5.241",
+    "192.112.36.4",
+    "198.97.190.53",
+    "192.36.148.17",
+    "192.58.128.30",
+    "193.0.14.129",
+    "199.7.83.42",
+    "202.12.27.33"
 ]
-
 DNS_SERVER_PORT = 53
 PACKET_SIZE = 4096
 HTTP_PORT = 80
@@ -64,14 +54,18 @@ def build_dns_packet(domain):
     return packet, q_name
 
 def send_dns_packet(packet, dns_ip):
-    start_time = time.time()
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.settimeout(10.0)
+    start_time = time.time()
     sock.sendto(packet, (dns_ip, DNS_SERVER_PORT))
-
-    response, client = sock.recvfrom(PACKET_SIZE)
-    end_time = time.time()
-    rtt = end_time - start_time
+    try:
+        response, client = sock.recvfrom(PACKET_SIZE)
+        end_time = time.time()
+        sock.close()
+    except socket.timeout:
+        sock.close()
+        return None, 0
+    rtt = (end_time - start_time) * 1000 #in ms
     return response, rtt
 
 def parse_dns_records(response, offset, count):
@@ -97,9 +91,6 @@ def parse_dns_records(response, offset, count):
 
         """
         A (1), NS (2), CNAME (5), SOA (6), PTR (12), MX (15), AAAA (28), SRV (33), and TXT (16)
-
-        NS: a.root-servers.net
-        A: 192.168.0.134
         """
         r_data = None
         #Need to advance offset in each case too *
@@ -111,163 +102,155 @@ def parse_dns_records(response, offset, count):
             offset += rdlength_val
             records[R_TYPE_VAL_TO_NAME[r_type_val]].append(r_data)
         elif r_type_val == 2 or r_type_val == 5:
-            name = ""
-            ns_offset = offset
-            while response[ns_offset] != 0:
-                if response[ns_offset] >= 192:
-                    pointer = struct.unpack("!H", response[ns_offset:ns_offset+2])[0] & 0x3FFF
-                    ns_offset = pointer
+            name_groups = []
+            name_offset = offset
+            while response[name_offset] != 0:
+                if response[name_offset] >= 192:
+                    # first 2 bits are 1s which means name is compressed
+                    # the next 14 bits are the offset to locate the actual name
+                    pointer = struct.unpack("!H", response[name_offset:name_offset+2])[0]
+                    pointer = pointer & 0x3FFF #mask to return only the last 14 bits
+                    name_offset = pointer
                     continue
-                label_len = response[ns_offset]
-                ns_offset += 1
-                name += response[ns_offset:ns_offset+label_len].decode() + "."
-                ns_offset += label_len
-            name = name.rstrip(".")
+                word_len = response[name_offset]
+                name_offset += 1
+                name_groups.append(str(response[name_offset:name_offset+word_len].decode()))
+                name_offset += word_len
+            name = ".".join(name for name in name_groups)
             r_data = name
             offset += rdlength_val
             records[R_TYPE_VAL_TO_NAME[r_type_val]].append(r_data)
         elif r_type_val == 28:
             #AAAA record
             ip_bytes = response[offset:offset+16]
-            ip_string = ".".join(str(i) for i in ip_bytes)
+            ipv6_groups = []
+            for i in range(0, 16, 2):
+                ipv6_groups.append(ip_bytes[i:i+2].hex())
+            ip_string = ":".join(hex for hex in ipv6_groups)
             r_data = ip_string
             offset += rdlength_val
             records[R_TYPE_VAL_TO_NAME[r_type_val]].append(r_data)
         else:
             offset += rdlength_val
     
-    
     return records, offset
+
+def print_records(sections):
+    combined_records = defaultdict(list)
+    for record_name in ["A", "NS", "CNAME", "AAAA"]:
+        for section in sections:
+            if record_name in section:
+                for val in section[record_name]:
+                    combined_records[record_name].append(val)
+
+    for record_name in combined_records:
+        for record in combined_records[record_name]:
+            print(f"{record_name} : {record}")
 
 def send_http_request(ip_string, domain):
     sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     sock.settimeout(10.0)
+    start_time = time.time()
     sock.connect((ip_string, HTTP_PORT))
-
     request = f"GET / HTTP/1.1\r\nHost: {domain}\r\nConnection: close\r\n\r\n"
+    print("--------------------------------------------")
+    print(f"Making HTTP request to {ip_string}")
+    print("--------------------------------------------")
     sock.sendall(request.encode())
     response = sock.recv(PACKET_SIZE)
-
-    return response
+    end_time = time.time()
+    sock.close()
+    rtt = (end_time - start_time) * 1000
+    return response, rtt
 
 def get_final_ip(domain):
     #-------------ROOT-----------
     packet, q_name = build_dns_packet(domain)
-    response, root_rtt = send_dns_packet(packet, ROOT_SERVERS[0])
+
+    for root_server in ROOT_SERVERS:
+        print("--------------------------------------------")
+        print(f"Querying {root_server} for {domain}")
+        print("--------------------------------------------")
+        response, root_rtt = send_dns_packet(packet, root_server)
+        if response is not None:
+            break
+    if response is None:
+        raise Exception("All root servers timed out")
     transaction_id, flags, question_count, answer_count, authority_count, additional_rr_count = struct.unpack("!HHHHHH", response[:12])
     offset = 12 + len(q_name) + 4 
 
     root_auth_records, offset = parse_dns_records(response, offset, authority_count) # No IP in the authority section
     root_addl_records, offset = parse_dns_records(response, offset, additional_rr_count)
-    
+    tld_ip_string = root_addl_records["A"][0]
+
+    print_records([root_auth_records, root_addl_records])
+    print(f"RTT: {root_rtt} ms")
     #print(f"TLD IP: {tld_ip_string}")
 
     #-------------TLD-----------
 
     packet, q_name = build_dns_packet(domain)
-    response, tld_rtt = send_dns_packet(packet, root_addl_records["A"][0])
+
+    print("--------------------------------------------")
+    print(f"Querying {tld_ip_string} for {domain}")
+    print("--------------------------------------------")
+    response, tld_rtt = send_dns_packet(packet, tld_ip_string)
     transaction_id, flags, question_count, answer_count, authority_count, additional_rr_count = struct.unpack("!HHHHHH", response[:12])
     offset = 12 + len(q_name) + 4 
 
     tld_auth_records, offset = parse_dns_records(response, offset, authority_count) # No IP in the authority section
     tld_addl_records, offset = parse_dns_records(response, offset, additional_rr_count)
 
-    #print(authority_records)
+    print_records([tld_auth_records, tld_addl_records])
+    print(f"RTT: {tld_rtt} ms")
 
     if "A" not in tld_addl_records and "NS" in tld_auth_records:
         #recursively query the NS domain
         # ns0229.secondary.cloudflare.com
-        print(f"name servers found {tld_auth_records['NS']}")
-        return get_final_ip(tld_auth_records["NS"][0])
+        ns_ip = get_final_ip(tld_auth_records["NS"][0])
+        packet, q_name = build_dns_packet(domain)
+        print("--------------------------------------------")
+        print(f"Querying {ns_ip} for {domain}")
+        print("--------------------------------------------")
+        response, auth_rtt = send_dns_packet(packet, ns_ip)
+        transaction_id, flags, question_count, answer_count, authority_count, additional_rr_count = struct.unpack("!HHHHHH", response[:12])
+        offset = 12 + len(q_name) + 4 
+        auth_ans_records, offset = parse_dns_records(response, offset, answer_count)
+        print_records([auth_ans_records])
+        print(f"RTT: {auth_rtt} ms")
+        final_ip = auth_ans_records["A"][0]
+        return final_ip
 
-    auth_ip = tld_addl_records["A"][0]
-
-
+    auth_ip_string = tld_addl_records["A"][0]
 
     #-------------AUTHORITATIVE-----------
+
+    print("--------------------------------------------")
+    print(f"Querying {auth_ip_string} for {domain}")
+    print("--------------------------------------------")
     #print(records)
     packet, q_name = build_dns_packet(domain)
-    response, auth_rtt = send_dns_packet(packet, auth_ip)
+    response, auth_rtt = send_dns_packet(packet, auth_ip_string)
     transaction_id, flags, question_count, answer_count, authority_count, additional_rr_count = struct.unpack("!HHHHHH", response[:12])
     offset = 12 + len(q_name) + 4 
 
     auth_ans_records, offset = parse_dns_records(response, offset, answer_count)
 
-    combined_records = defaultdict(list)
-    for record_name in ["A", "NS", "CNAME", "AAAA"]:
-        if record_name in root_auth_records:
-            for val in root_addl_records[record_name]:
-                combined_records[record_name].append(val)
-        if record_name in root_addl_records:
-            for val in root_addl_records[record_name]:
-                combined_records[record_name].append(val)
-        if record_name in tld_addl_records:
-            for val in tld_addl_records[record_name]:
-                combined_records[record_name].append(val)
-        if record_name in tld_auth_records:
-            for val in tld_auth_records[record_name]:
-                combined_records[record_name].append(val)
-        if record_name in auth_ans_records:
-            for val in auth_ans_records[record_name]:
-                combined_records[record_name].append(val)
-        
-    print(f"root rtt = {root_rtt},\ntld rtt = {tld_rtt},\nauth rtt = {auth_rtt}\n\n")
-    
-    print(combined_records)
+    print_records([auth_ans_records])
+    print(f"RTT: {auth_rtt} ms")
 
 
-    final_ip = auth_ans_records["A"][0]
-    return final_ip
+    final_ip_string = auth_ans_records["A"][0]
+    return final_ip_string
 
 def solve(domain):
     final_ip = get_final_ip(domain)
-    response = send_http_request(final_ip, domain)
-    #print(response)
-
-
-
-    """
-    [ 12-byte HEADER ]
-    [ QUESTION section:
-        q_name (variable length)
-        For wikipedia.org
-        q_name should be "9 wikipedia 3 org 0"
-                          1 9         1 3   1 = 15
-        q_type 2 bytes
-        q_class 2 bytes
-    ]
-    [ ANSWER section ]
-    [ AUTHORITY section 
-
-        DNS resource records:
-        NAME        (variable, can be compressed)
-            11000000 = 192 = NAME is always 2 bytes
-
-        TYPE        2 bytes
-        CLASS       2 bytes
-        TTL         4 bytes
-        RDLENGTH    2 bytes 
-            The length of RDATA
-        RDATA       variable length (RDLENGTH bytes)
-    
-    ]
-    [ ADDITIONAL section 
-        DNS resource records:
-        NAME        (variable, can be compressed)
-            11000000 = 192 = NAME is always 2 bytes
-
-        TYPE        2 bytes
-        CLASS       2 bytes
-        TTL         4 bytes
-        RDLENGTH    2 bytes 
-            The length of RDATA
-        RDATA       variable length (RDLENGTH bytes)
-    ]
-    """
-
-    #print(transaction_id)
-
-    pass
+    http_response, http_rtt = send_http_request(final_ip, domain)
+    http_lines = http_response.decode().split("\r\n")
+    status_line = http_lines[0]
+    status_code = status_line.split(" ")[1]
+    print(status_code)
+    print(f"RTT: {http_rtt} ms")
 
 if __name__ == '__main__':
     if len(sys.argv) != 2:
